28ec756b37efabcf637fae797e52907819c6691b
Raghuram Pulijala <raghuram.pulijala@gmail.com>
2013-10-24 11:26:56 -0400

    FriendBook first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriendBook::Application.config.secret_key_base = '35b06ac31d830cbf3959407e71e07de1661b96a041fdc9092686258ebfc0e4b42345841a80d376f73b0ff9041080ba735dbbd6d270c72833a86dfa042bdaac39'
